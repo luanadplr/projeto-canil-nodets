b27512008d3dd43dc119d10c4c9131905c912229
Luana de Paula L Ramos <luanadepaulalr@gmail.com>
2025-09-01 17:17:10 -0300

    setup-add-tsc

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 import { createMenuObject } from '../helpers/createMenuObject'
-import { Pet } from '../models/pet'
+import { Pet } from '../models/Pet'
 
 export const home = (request: Request, response: Response) => {
     let list = Pet.getAll()
--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express"
 import { createMenuObject } from '../helpers/createMenuObject'
-import { Pet } from '../models/pet'
+import { Pet } from '../models/Pet'
 
 export const search = (request: Request, response: Response) => {
     let query: string = request.query.q as string
